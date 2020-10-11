17d6a5775bd8c83fc6ccc1dadf59e7bc0fb5070b
Papiando <info@papiando.com>
2020-10-10 23:51:58 -0400

    Delete ArticleController.mjs

--- test/ArticleController.mjs
+++
@@ -1,5 +0,0 @@
-import Entity from '../module/Entity.mjs';
-import Controller from '../module/Controller.mjs';
-import ArticleController from '../module/ArticleController.mjs';
-
-let ent = new ArticleController();
